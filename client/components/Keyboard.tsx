eadb12781da7b7b1fc966c3d2f92be4f61ef51b6
Brandon Hancock <brandon9598@gmail.com>
2022-08-21 12:16:15 -0400

    removed unused functino

--- client/components/Keyboard.tsx
+++ client/components/Keyboard.tsx
@@ -1,5 +1,5 @@
 import { KEYBOARD_KEYS, KeyboardKeyValuePair } from "../utils/constants";
-import React, { useEffect } from "react";
+import React from "react";
 
 import { LetterGuess } from "../types/LetterGuess";
 import { useGame } from "../context/gameContext";
